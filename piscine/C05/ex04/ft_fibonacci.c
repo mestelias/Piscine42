8dfd54a79a3c52743f9d87d7ffa9b2f31513ce01
mestelias <60406101+mestelias@users.noreply.github.com>
2020-07-19 14:03:53 +0200

    Add files via upload

---
+++ piscine/C05/ex04/ft_fibonacci.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emestour <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 13:48:06 by emestour          #+#    #+#             */
+/*   Updated: 2020/07/15 11:08:30 by emestour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1 || index == 2)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
---
+++ piscine/C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emestour <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 15:09:38 by emestour          #+#    #+#             */
+/*   Updated: 2020/07/14 16:01:25 by emestour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_is_prime(int nb)
+{
+	int		i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int		ft_find_next_prime(int nb)
+{
+	if (nb < 2)
+		return (2);
+	while (ft_is_prime(nb) == 0)
+	{
+		nb++;
+	}
+	return (nb);
+}
